26d01eb7973d7d028659f108e00e66e8e0d06f32
MM <mm@inlead.dk>
2016-10-24 15:35:38 +0300

    EASYOPAC-258 - Port backside cover to separate module for ding2.

---
+++ lib/addi-client-backside/AdditionalInformationBackside.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * AdditionalInformationBackside class.
+ */
+
+class AdditionalInformationBackside{
+
+  public $backsideUri;
+
+  public function __construct($backsideUri) {
+    $this->backsideUri = $backsideUri;
+  }
+
+}
---
+++ lib/addi-client-backside/AdditionalInformationBacksideService.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * AdditionalInformationBacksideService class.
+ */
+
+module_load_include('php', 'ting_covers_addi', 'lib/addi-client/AdditionalInformationService');
+
+class AdditionalInformationBacksideService extends AdditionalInformationService {
+
+  protected $wsdlUrl;
+  protected $username;
+  protected $group;
+  protected $password;
+
+  public function __construct($wsdl_url, $username, $group, $password) {
+    parent::__construct($wsdl_url, $username, $group, $password);
+  }
+
+  protected function extractAdditionalInformation($id_name, $response) {
+    $additional_informations = array();
+
+    foreach ($response->identifierInformation as $info) {
+      $cover_image = isset($info->coverImage) ? $info->coverImage : FALSE;
+
+      $back_page = isset($info->backPage->_) ? $info->backPage->_ : FALSE;
+
+      if (isset($info->identifierKnown) && $info->identifierKnown && $cover_image && $back_page) {
+        $additional_info = new AdditionalInformationBackside($back_page);
+        $additional_informations[$info->identifier->$id_name] = $additional_info;
+      }
+    }
+
+    return $additional_informations;
+  }
+
+}
