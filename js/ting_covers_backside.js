aad3f70597f42a379867d2c5dc88328d4073f219
MM <mm@inlead.dk>
2017-01-23 16:27:42 +0200

    EASYOPAC-258 - Move code into Drupal.behaviors instead of document ready.

--- js/ting_covers_backside.js
+++ js/ting_covers_backside.js
@@ -1,52 +1,55 @@
 (function($) {
   "use strict";
 
-  var backside_popup_data = {};
+  Drupal.behaviors.backside = {
+    attach: function(context) {
 
-  // Helper function to get information about a given cover place holder.
-  var ting_covers_backside_extract_data = function(e) {
-    var classname = $(e).attr('class');
-    var imageStyle = classname.match(/ting-cover-style-(\S+)/);
-    var id = classname.match(/ting-cover-object-id-(\S+)/);
-    return {
-      local_id : id[1],
-      image_style : imageStyle[1]
-    };
-  };
+      var backside_popup_data = {};
 
-  $(document).ready(function () {
-    // Assemble information regarding covers.
-    var cover_data = [];
+      // Helper function to get information about a given cover place holder.
+      var ting_covers_backside_extract_data = function(e) {
+        var classname = $(e).attr('class');
+        var imageStyle = classname.match(/ting-cover-style-(\S+)/);
+        var id = classname.match(/ting-cover-object-id-(\S+)/);
+        return {
+          local_id : id[1],
+          image_style : imageStyle[1]
+        };
+      };
 
-    // Extract cover information from the dom.
-    $('div.ting-cover:not(.ting-backside-processed)').each(function(index, element) {
-      cover_data.push(ting_covers_backside_extract_data(element));
-    });
+      // Load PDF file on modal open.
+      $(document).on('reveal:open', 'div[id^="reveal-cover-back-"]', function () {
+        $(this).find('object').show();
+      });
 
-    if (cover_data.length === 0) {
-      return false;
-    }
+      // Assemble information regarding covers.
+      var cover_data = [];
 
-    $.ajax({
-      url: Drupal.settings.basePath + 'ting/covers/backside',
-      type: 'POST',
-      data: {
-        coverData: cover_data
-      },
-      dataType: 'json',
-      success: function (coverData) {
-        $.each(coverData, function(id, data) {
-          var $current = $('#work-cover-' + id).parent();
+      // Extract cover information from the dom.
+      $('div.ting-cover:not(.ting-backside-processed)').each(function(index, element) {
+        cover_data.push(ting_covers_backside_extract_data(element));
+      });
 
-          $current.html('');
-          $(data.data).appendTo($current);
-        });
+      if (cover_data.length === 0) {
+        return false;
       }
-    });
 
-    // Load PDF file on modal open.
-    $(document).on('reveal:open', 'div[id^="reveal-cover-back-"]', function () {
-      $(this).find('object').show();
-    });
-  });
+      $.ajax({
+        url: Drupal.settings.basePath + 'ting/covers/backside',
+        type: 'POST',
+        data: {
+          coverData: cover_data
+        },
+        dataType: 'json',
+        success: function (coverData) {
+          $.each(coverData, function(id, data) {
+            var $current = $('#work-cover-' + id).parent();
+
+            $current.html('');
+            $(data.data).appendTo($current);
+          });
+        }
+      });
+    }
+  };
 }(jQuery));
