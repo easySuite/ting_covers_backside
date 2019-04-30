d8231506d86250157e756b6653641e4cb6460ada
Alex <bozeak@gmail.com>
2019-04-30 10:34:31 +0300

    EASYOPAC-1069 - Commiting work done. Need cleanup.

--- js/ting_covers_backside.js
+++ js/ting_covers_backside.js
@@ -8,13 +8,51 @@
     };
   };
 
+
+  // Select the node that will be observed for mutations
+  // var targetNode = document.getElementsByClassName('cover-front');
+//   var targetNode = document.getElementsByClassName('cover-front');
+//   console.log(targetNode)
+//
+//   // Options for the observer (which mutations to observe)
+//   var config = { attributes: true, childList: true, subtree: true };
+//
+//   // Callback function to execute when mutations are observed
+//   var callback = function(mutationsList, observer) {
+//     for(var mutation of mutationsList) {
+//       if (mutation.type == 'childList') {
+//         console.log('A child node has been added or removed.');
+//       }
+//       else if (mutation.type == 'attributes') {
+//         console.log('The ' + mutation.attributeName + ' attribute was modified.');
+//       }
+//     }
+//   };
+//
+// // Create an observer instance linked to the callback function
+//   var observer = new MutationObserver(callback);
+//
+// // Start observing the target node for configured mutations
+//   observer.observe(targetNode, config);
+//   console.log(observer)
+//
+// // Later, you can stop observing
+//   observer.disconnect();
+
   Drupal.behaviors.ting_covers_backside = {
-    attach: function(context) {
-      // Don't show ting search overlay when covers opens in popup.
-      $('body', context).on('click', 'a.reveal-cover', function () {
-        var reveal_id = $(this).attr('data-reveal-id');
-        $('#' + reveal_id).reveal();
-        return false;
+    attach: function(context, settings) {
+      var cover_loaded = $(document, context).find('.cover-front');
+
+      if ($(cover_loaded).hasClass('cboxElement')) {
+        $('.work-cover-selector').show();
+      }
+
+      $('.cover-front').colorbox();
+
+      $('.cover-back').colorbox({
+        iframe: true,
+        height: '90%',
+        width: '590px',
       });
 
       // Assemble information regarding covers.
@@ -25,7 +63,7 @@
       });
       if (cover_data.length > 0) {
         $.ajax({
-          url: Drupal.settings.basePath + 'ting/covers/backside',
+          url: settings.basePath + settings.pathPrefix + 'ting/covers/backside',
           type: 'POST',
           data: {
             coverData: cover_data
@@ -35,46 +73,6 @@
             $.each(coverData, function (id, data) {
               $('div.ting-object').find('[data-ting-cover-object-id="' + id + '"]').next('.backside-covers-wrapper').replaceWith(data);
             });
-
-            // As we have problems with positioning of modal on non ting_object
-            // pages, we will move div with cover content before the body
-            // closing tag so it is show normally.
-            var reveal_content = $('.reveal-modal');
-            if (reveal_content.length !== 0) {
-              reveal_content.appendTo('body');
-            }
-
-            // Don't display covers icons on carousels.
-            var ding_carousel_items = $('.ding-carousel-item').once();
-            if (ding_carousel_items.length !== 0) {
-              $.map(ding_carousel_items, function(item) {
-                var link = $(item).find('a')[0];
-                if ($(link).children().hasClass('work-cover-selector')) {
-                  $('.ding-carousel .work-cover-selector').remove();
-                }
-              });
-            }
-          }
-        });
-
-        // Load PDF file on modal open.
-        $(document, context).on('reveal:open', '.reveal-modal', function () {
-          var $wrapper;
-          var hash = $(this).data('hash');
-          var uri = $(this).children().children().data('uri');
-
-          if (hash) {
-            var options = {
-              height: "100%",
-              width: "590px",
-              pdfOpenParams: {view: "FitV", page: '1'}
-            };
-
-            options.forcePDFJS = true;
-            options.PDFJS_URL = Drupal.settings.basePath + 'profiles/ding2/libraries/pdfjs/web/viewer.html';
-
-            $wrapper = $('#reveal-cover-back-' + hash + ' .reveal-cover-back-image');
-            PDFObject.embed(uri, $wrapper, options);
           }
         });
       }
