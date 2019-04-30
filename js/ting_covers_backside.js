15475ab7c4bab4e864816ea0f17207837311a427
Alex <bozeak@gmail.com>
2019-04-30 12:48:20 +0300

    EASYOPAC-1069 - Unify usage of modal/overlay.

--- js/ting_covers_backside.js
+++ js/ting_covers_backside.js
@@ -8,37 +8,6 @@
     };
   };
 
-
-  // Select the node that will be observed for mutations
-  // var targetNode = document.getElementsByClassName('cover-front');
-//   var targetNode = document.getElementsByClassName('cover-front');
-//   console.log(targetNode)
-//
-//   // Options for the observer (which mutations to observe)
-//   var config = { attributes: true, childList: true, subtree: true };
-//
-//   // Callback function to execute when mutations are observed
-//   var callback = function(mutationsList, observer) {
-//     for(var mutation of mutationsList) {
-//       if (mutation.type == 'childList') {
-//         console.log('A child node has been added or removed.');
-//       }
-//       else if (mutation.type == 'attributes') {
-//         console.log('The ' + mutation.attributeName + ' attribute was modified.');
-//       }
-//     }
-//   };
-//
-// // Create an observer instance linked to the callback function
-//   var observer = new MutationObserver(callback);
-//
-// // Start observing the target node for configured mutations
-//   observer.observe(targetNode, config);
-//   console.log(observer)
-//
-// // Later, you can stop observing
-//   observer.disconnect();
-
   Drupal.behaviors.ting_covers_backside = {
     attach: function(context, settings) {
       var cover_loaded = $(document, context).find('.cover-front');
